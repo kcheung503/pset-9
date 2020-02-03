8dbebc0b2c533f04a701779cf721b73b8c42c197
Kyle Cheung <kcheung2@ucvts.tec.nj.us>
2020-02-03 09:08:57 -0500

    beginning exercise 10

--- src/Exercises.java
+++ src/Exercises.java
@@ -209,7 +209,19 @@ public class Exercises {
 				}
 
 	public int clumps(ArrayList<String> values) {
-		// write your code here
+		if (values == null) {
+			return -1;
+		}
+		for (int i = 0; i < values.size(); i++) {
+			if (values.get(i) == null) {
+				return -1;
+			}
+		}
+
+		boolean isSame = false;
+		boolean prevSame = false;
+		String previous = "";
+		int clumps = 0;
 
 		return -1;		// default return value to ensure compilation
 	}
